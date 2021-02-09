db35842792d23b27920e822e7ab39f006372bc4e
Hao-Ting Li <maniac@csie.io>
2021-02-09 08:51:14 +0800

    feat: add robust tests

--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -1,9 +1,30 @@
+import pytest
+
 from cns11643.lookup import LookupComponent
 
 
-def test_lookup_component():
-    lookup_component = LookupComponent()
+@pytest.fixture
+def lookup_component():
+    return LookupComponent()
+
+
+def test_lookup_component_1(lookup_component):
     word = '雷'
-    component = lookup_component(word=word)
+    components = lookup_component(word=word)
     expected = [454, 301]
-    assert component == expected
+    assert components == expected
+
+
+def test_lookup_component_2(lookup_component):
+    word = '永'
+    components = lookup_component(word=word)
+    expected = [342]
+    assert components == expected
+
+
+# test when the word has multiple component list
+def test_lookup_component_3(lookup_component):
+    word = '衛'
+    components = lookup_component(word=word)
+    expected = [127, 151, 119, 111, 19, 10]
+    assert components == expected
